2810ce6e5529f0badcf0d147fee4ecfce52cc221
Bimsara Geethachapa <139919035+Bimsarasmp@users.noreply.github.com>
2024-04-17 23:57:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000, threaded=True, **{'request_timeout': 120})
